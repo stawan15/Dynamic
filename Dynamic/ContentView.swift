23e6e17e7584fca43cd85a40b5347701c1934b64
stawan15 <sutipojr@gmail.com>
2026-08-01 17:23:07 +0700

    Initial Commit

---
+++ Dynamic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dynamic
+//
+//  Created by stawan15 on 1/8/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dynamic/DynamicApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicApp.swift
+//  Dynamic
+//
+//  Created by stawan15 on 1/8/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct DynamicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
